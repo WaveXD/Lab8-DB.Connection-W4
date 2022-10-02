e8b726e8cffbbbeb6cc60c26d521c625022e9349
WaveXD <109848995+WaveXD@users.noreply.github.com>
2022-10-02 14:34:26 +0700

    Add files via upload

---
+++ Lab8-DB-4.php
@@ -0,0 +1,26 @@
+<?php include "connect.php" ?>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <form>
+        <input type="text" name="username">
+        <input type="submit" value="search">
+    </form>
+    <?php
+    $stmt = $pdo->prepare("SELECT * FROM member WHERE username LIKE ?");
+    if(!empty($_GET)){
+        $value = '%'.$_GET["username"].'%';
+    }
+    $stmt ->bindParam(1,$value);
+    $stmt->execute();
+    while ($row = $stmt->fetch()) { 
+    ?>
+    ชื่อสมาชิก: <?=$row ["name"]?><br>
+    ที่อยู่: <?=$row ["address"]?><br>
+    อีเมล: <?=$row ["email"]?><br>
+    <img src="member_photo/<?=$row["username"]?>.jpg" height = 100px><hr>
+    <?php } ?>
+</body>
+</html>
